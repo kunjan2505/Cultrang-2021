6d51ba3d8b214a008dd93a97fd2c1b9838d849be
arpitm859 <arpitchatting@gmail.com>
2021-03-11 12:38:08 +0530

    added about us

---
+++ src/pages/about-us/imageUrl.js
@@ -0,0 +1,67 @@
+
+export const IMAGE_URL = [
+	{
+		id: 1,
+		url: 'images/1.JPG',
+	},
+	{
+		id: 2,
+		url: 'images/2.JPG',
+	},
+	{
+		id: 3,
+		url: 'images/3.JPG',
+	},
+	{
+		id: 4,
+		url: 'images/4.JPG',
+	},
+	{
+		id: 5,
+		url: 'images/5.JPG',
+	},
+	{
+		id: 6,
+		url: 'images/6.JPG',
+	},
+	{
+		id: 7,
+		url: 'images/7.JPG',
+	},
+	{
+		id: 8,
+		url: 'images/8.JPG',
+	},
+	{
+		id: 9,
+		url: 'images/9.JPG',
+	},
+	{
+		id: 10,
+		url: 'images/10.JPG',
+	},
+	{
+		id: 11,
+		url: 'images/11.JPG',
+	},
+	{
+		id: 12,
+		url: 'images/12.JPG',
+	},
+	{
+		id: 13,
+		url: 'images/13.JPG',
+	},
+	{
+		id: 14,
+		url: 'images/14.JPG',
+	},
+	{
+		id: 15,
+		url: 'images/15.JPG',
+	},
+	{
+		id: 16,
+		url: 'images/16.JPG',
+	},
+];
