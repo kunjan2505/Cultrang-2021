afec0ade873e28696f8eaf826277f389acb10c9e
arpitm859 <arpitchatting@gmail.com>
2021-03-13 14:26:51 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import Home from './pages/home/home'
 import AboutUs from './pages/about-us/aboutUs';
 import ContactUs from './pages/contactUs/contactUs';
 import Register from './pages/register/register';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home className="home-page" />
-      <AboutUs />
-      <ContactUs />
-      <Register />
-    </div>
-  );
+			<div className='App'>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/about-us' component={AboutUs} />
+					<Route exact path='/contact-us' component={ContactUs} />
+					<Route exact path='/events' />
+					<Register />
+				</Switch>
+			</div>
+		);
 }
 
 export default App;
