966a32ad49138442a1d8ab7f13bc70b4e4070004
arpitm859 <arpitchatting@gmail.com>
2021-03-22 22:55:51 +0530

    .

--- src/pages/about-us/imageUrl.js
+++ src/pages/about-us/imageUrl.js
@@ -1,68 +1,72 @@
-
-export const IMAGE_URL = [
-	{
-		id: 1,
-		url: 'images/1.jpg',
+export const IMAGE_URL = {
+	photos: [
+		{
+			id: 1,
+			url: 'images/1.jpg',
+		},
+		{
+			id: 2,
+			url: 'images/2.jpg',
+		},
+		{
+			id: 3,
+			url: 'images/3.jpg',
+		},
+		{
+			id: 4,
+			url: 'images/4.jpg',
+		},
+		// {
+		// 	id: 5,
+		// 	url: 'images/5.jpg',
+		// },
+		{
+			id: 6,
+			url: 'images/6.jpg',
+		},
+		// {
+		// 	id: 7,
+		// 	url: 'images/7.jpg',
+		// },
+		{
+			id: 8,
+			url: 'images/8.jpg',
+		},
+		{
+			id: 9,
+			url: 'images/9.jpg',
+		},
+		{
+			id: 10,
+			url: 'images/10.jpg',
+		},
+		{
+			id: 11,
+			url: 'images/11.jpg',
+		},
+		{
+			id: 12,
+			url: 'images/12.jpg',
+		},
+		// {
+		// 	id: 13,
+		// 	url: 'images/13.jpg',
+		// },
+		{
+			id: 14,
+			url: 'images/14.jpg',
+		},
+		{
+			id: 15,
+			url: 'images/15.jpg',
+		},
+		// {
+		// 	id: 16,
+		// 	url: 'images/16.jpg',
+		// },
+	],
+	about: {
+		content:
+			'IIT Goa, into its 5th year of existence only has already grown to be a much sought-after institution for engineering students and a melting-pot of ideas.We are nestled into the vast and beautiful expanse of GEC campus in Farmagudi, Goa with a vibrant population of over three thousand students and five hundred faculty members and staffs. While we strive to achieve excellence in academic endeavors, we are certainly touched and influenced by the rich and unique culture of Goa which is exactly what we want to celebrate through the Cultural fest of IIT Goa, Cult Rang. CultRang is a celebration of the massive diversity that characterises our country. sIt aims at instilling a sense of collective engagement by encouraging participation from several institutes all across the country. It consists of exciting events, competitions and workshops featuring experts in related fields, focusing on domains of performing arts that recognise the role of various cultures in our everyday indulgences.',
 	},
-	{
-		id: 2,
-		url: 'images/2.jpg',
-	},
-	{
-		id: 3,
-		url: 'images/3.jpg',
-	},
-	{
-		id: 4,
-		url: 'images/4.jpg',
-	},
-	// {
-	// 	id: 5,
-	// 	url: 'images/5.jpg',
-	// },
-	{
-		id: 6,
-		url: 'images/6.jpg',
-	},
-	// {
-	// 	id: 7,
-	// 	url: 'images/7.jpg',
-	// },
-	{
-		id: 8,
-		url: 'images/8.jpg',
-	},
-	{
-		id: 9,
-		url: 'images/9.jpg',
-	},
-	{
-		id: 10,
-		url: 'images/10.jpg',
-	},
-	{
-		id: 11,
-		url: 'images/11.jpg',
-	},
-	{
-		id: 12,
-		url: 'images/12.jpg',
-	},
-	// {
-	// 	id: 13,
-	// 	url: 'images/13.jpg',
-	// },
-	{
-		id: 14,
-		url: 'images/14.jpg',
-	},
-	{
-		id: 15,
-		url: 'images/15.jpg',
-	},
-	// {
-	// 	id: 16,
-	// 	url: 'images/16.jpg',
-	// },
-];
-
+};
