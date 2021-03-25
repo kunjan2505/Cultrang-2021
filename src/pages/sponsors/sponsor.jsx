5b4fc83ba383cf824423f12843fb8d5abc8c0d35
Raghvendra Singh <raghvendra.singh.19031@iitgoa.ac.in>
2021-03-25 21:44:25 +0530

    sponsors Optimized

--- src/pages/sponsors/sponsor.jsx
+++ src/pages/sponsors/sponsor.jsx
@@ -17,6 +17,7 @@ import yi from './logos/yi.png';
 import cloudVect from './sponsor_cloud.svg';
 import bottomVect from './sponsor_bottom.png';
 import cb from './logos/cb.png';
+import nBard from './logos/nbard.png';
 
 class Sponsor extends React.Component {
 	
@@ -29,32 +30,66 @@ class Sponsor extends React.Component {
 					<div className='heading-container-sponsor'>
 						<h1 className='heading'>SPONSORS</h1>
 					</div>
-					<Divider id='divi-der'>TITLE SPONSOR</Divider>
-					<div className='row1'>
-						<a href='https://isb.alienbrains.in/2021' target='_blank'>
-							<div
-								className='card'
-								id='test'
-								style={{ width: '25vw', marginTop: '-7vw' }}
-							>
-								<img src={isb} class='card-img-top' alt='...' id='spons-image' />
-							</div>
-						</a>
-					</div>
-					<Divider id='divi-der' style={{ marginTop: '-4vw' }}>
-						CO-SPONSORS
-					</Divider>
+
 					<div className='row2'>
-						<a href='https://www.deltastore.in/' target='_blank'>
-							<div className='card' style={{ width: '10vw' }}>
-								<img src={delta} class='card-img-top' alt='...' id='spons-image' />
+						<a href='https://www.deltastore.in/' target='_blank' id="t1" style={{flex: '1 0 10vw'}}>
+							<div className='card' >
+								<div class='card-body'>
+									<h5 class='card-title'>CO-SPONSOR</h5>
+								</div>
+								<img style={{ width: '10vw' }} src={delta} class='card-img-top' alt='...' id='spons-image' />
 							</div>
 						</a>
-						<div className='card' style={{ width: '10vw' }}>
-							<img src={sponsorBull} class='card-img-top' alt='...' id='spons-image' />
+						<a href='https://isb.alienbrains.in/2021' target='_blank' id='t2' style={{flex: '2 0 20vw'}}>
+							<div className='card' id='test' >
+								<div class='card-body'>
+									<h5 class='card-title'>TITLE SPONSOR</h5>
+								</div>
+								<img src={isb} class='card-img-top' alt='...' id='spons-image' style={{ width: '25vw', marginTop: '-7vw' }}/>
+							</div>
+						</a>
+						<a href='#' id='t3' style={{flex: '1 0 10vw'}}>
+						<div className='card' >
+							<div class='card-body'>
+								<h5 class='card-title' style={{paddingBottom: '0px !important'}}>SPONSORSHIP PARTNER</h5>
+							</div>
+							<img src={sponsorBull} class='card-img-top' alt='...' id='spons-image' style={{ width: '10vw'}}/>
+						</div></a>
+					</div>
+					<Divider id='divi-der' className='media_divider'>
+						MEDIA SPONSORS
+					</Divider>
+					<div className='column3'>
+						<div className='row41'>
+							<a href='https://youthincmag.com/' target='_blank'>
+								<div className='card' style={{ width: '10vw' }} id='spons-image'>
+									<img src={yi} class='card-img-top' alt='...' />
+									<div class='card-body'>
+										<h5 class='card-title'>Youth Media Partner</h5>
+									</div>
+								</div>
+							</a>
+
+							<a href='https://www.noticebard.com/' target='_blank'>
+								<div className='card' style={{ width: '10vw' }} id='spons-image'>
+									<img src={nBard} class='card-img-top' alt='...' />
+									<div class='card-body'>
+										<h5 class='card-title'>Media Partner</h5>
+									</div>
+								</div>
+							</a>
+
+							<a href='https://incampus.in/' target='_blank'>
+								<div className='card' style={{ width: '10vw' }} id='spons-image'>
+									<img src={inCampus} class='card-img-top' alt='...' />
+									<div class='card-body'>
+										<h5 class='card-title'>Outreach Partner</h5>
+									</div>
+								</div>
+							</a>
 						</div>
 					</div>
-					<Divider id='divi-der'>INDIVIDUAL SPONSORS</Divider>
+					<Divider id='divi-der'>OTHER SPONSORS</Divider>
 					<div className='column4'>
 						<div className='row41'>
 							<a href='https://www.marktechpost.com/' target='_blank'>
@@ -65,18 +100,18 @@ class Sponsor extends React.Component {
 									</div>
 								</div>
 							</a>
-							<a href='http://www.townscript.com/' target='_blank'>
-								<div className='card' style={{ width: '10vw' }} id='spons-image'>
-									<img src={town} alt='...' class='card-img-top' />
-									<div>
-										<h5 class='card-title'>Platform Partner</h5>
-									</div>
+
+							<div className='card' style={{ width: '10vw' }} id='spons-image'>
+								<img src={sociana} class='card-img-top' alt='...' />
+								<div class='card-body'>
+									<h5 class='card-title'>Registration Partner</h5>
 								</div>
-							</a>
+							</div>
+							
 							<a href='https://www.inmovidutech.com/' target='_blank'>
 								<div className='card' style={{ width: '10vw' }} id='spons-image'>
 									<img src={inMovidu} class='card-img-top' alt='...' />
-									<div class='card-body'>
+									<div class='card-body' style={{width: '30vw'}}>
 										<h5 class='card-title'>
 											Official Industrial Training & Live Project Partner
 										</h5>
@@ -102,7 +137,7 @@ class Sponsor extends React.Component {
 								</div>
 							</a>
 							<a href='https://codingblocks.com/' target='_blank'>
-								<div className='card' style={{ width: '10vw' }} id='spons-image'>
+								<div className='card' style={{ width: '10vw', marginTop: '1vw' }} id='spons-image'>
 									<img src={cb} class='card-img-top' alt='...' />
 									<div class='card-body'>
 										<h5 class='card-title'>Coding Partner</h5>
@@ -119,12 +154,7 @@ class Sponsor extends React.Component {
 									</div>
 								</div>
 							</a>
-							<div className='card' style={{ width: '10vw' }} id='spons-image'>
-								<img src={sociana} class='card-img-top' alt='...' />
-								<div class='card-body'>
-									<h5 class='card-title'>Registration Partner</h5>
-								</div>
-							</div>
+							
 							<a href='http://www.tossidoonline.in/' target='_blank'>
 								<div className='card' style={{ width: '10vw' }} id='spons-image'>
 									<img src={tossido} class='card-img-top' alt='...' />
@@ -135,29 +165,7 @@ class Sponsor extends React.Component {
 							</a>
 						</div>
 					</div>
-					<Divider id='divi-der' className='media_divider'>
-						MEDIA SPONSORS
-					</Divider>
-					<div className='column3'>
-						<div className='row41'>
-							<a href='https://youthincmag.com/' target='_blank'>
-								<div className='card' style={{ width: '10vw' }} id='spons-image'>
-									<img src={yi} class='card-img-top' alt='...' />
-									<div class='card-body'>
-										<h5 class='card-title'>Youth Media Partner</h5>
-									</div>
-								</div>
-							</a>
-							<a href='https://incampus.in/' target='_blank'>
-								<div className='card' style={{ width: '10vw' }} id='spons-image'>
-									<img src={inCampus} class='card-img-top' alt='...' />
-									<div class='card-body'>
-										<h5 class='card-title'>Outreach Partner</h5>
-									</div>
-								</div>
-							</a>
-						</div>
-					</div>
+					
 				</div>
 				<div id='bottomVector'>
 					<img id='bottomPng' src={bottomVect} />
